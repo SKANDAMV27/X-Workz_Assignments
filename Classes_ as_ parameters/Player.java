8033f4fa2c38d7b5df4f592ecc047f6963eabe08
Skanda <skandagowda0@gmail.com>
2025-03-18 23:51:39 +0530

    Simple Code

---
+++ Classes_ as_ parameters/Player.java
@@ -0,0 +1,5 @@
+class Player
+void play()
+{
+	System.out.println("Play");
+}
---
+++ Classes_ as_ parameters/Runner.java
@@ -0,0 +1,10 @@
+class Runner{
+	public static void main(String... args){
+		
+		Display display=new Display();
+		Shop shop=new Shop();
+		
+		
+		shop.ads(display);
+	}
+}
